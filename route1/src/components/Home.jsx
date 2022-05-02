f89b873f92459b61e70e88efd3ac535a4d1c1074
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2022-05-03 00:28:58 +0530

    route

---
+++ route1/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Getuser } from './Login'
+import {  Login } from './Login'
+
+
+
+export const Home = () => {
+ 
+
+  return (
+    <div>
+     <h1>{Getuser}</h1>
+    </div>
+  )
+}
