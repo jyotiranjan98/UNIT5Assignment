51176c54bc7917814045efb3fbb93941a7266400
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2022-05-11 16:37:21 +0530

    api

---
+++ Theme-Context/src/styles.js
@@ -0,0 +1,10 @@
+export const styles = {
+    dark: {
+       backgroundColor: "rgb(29, 25, 87)",
+       color: "white",
+    },
+    light: {
+       backgroundColor: "rgb(207, 242, 190)",
+       color: "black",
+    },
+ };
